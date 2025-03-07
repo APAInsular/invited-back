276b2315d190e4ece7da227fcd1114b34161e727
LucasVega2004 <luquitasvegablanco@gmail.com>
2025-03-07 14:40:58 +0000

    eliminar barra cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],  // Permitir solo las rutas que empiezan con /api/
     'allowed_methods' => ['*'],  // Permitir todos los métodos HTTP
-    'allowed_origins' => ['http://localhost:3000','https://invited.es/*'],  // Agregar el dominio de tu frontend
+    'allowed_origins' => ['http://localhost:3000','https://invited.es'],  // Agregar el dominio de tu frontend
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],  // Permitir todos los headers
     'exposed_headers' => [],
