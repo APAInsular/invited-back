27dfec6631161f0e0fb131d13d47639da4343fda
LucasVega2004 <luquitasvegablanco@gmail.com>
2025-03-27 09:27:14 +0000

    cambio 3

--- resources/views/emails/contact_message.blade.php
+++ resources/views/emails/contact_message.blade.php
@@ -6,9 +6,9 @@
 </head>
 <body>
     <h2>Nuevo mensaje de contacto</h2>
-    <p><strong>Nombre:</strong> {{ $data['name'] }}</p>
-    <p><strong>Correo:</strong> {{ $data['email'] }}</p>
+    <p><strong>Nombre:</strong> {{ $formData['name'] }}</p>
+    <p><strong>Correo:</strong> {{ $formData['email'] }}</p>
     <p><strong>Mensaje:</strong></p>
-    <p>{{ $data['message'] }}</p>
+    <p>{{ $formData['message'] }}</p>
 </body>
 </html>
